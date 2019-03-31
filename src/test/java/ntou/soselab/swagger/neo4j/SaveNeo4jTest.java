38f16a32d5d6f201458beaa8e7526384f09f546b
許銘仁 <surprised128@gmail.com>
2019-03-31 22:31:05 +0800

    fix at most 20 operation and add LDATest

--- src/test/java/ntou/soselab/swagger/neo4j/SaveNeo4jTest.java
+++ src/test/java/ntou/soselab/swagger/neo4j/SaveNeo4jTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SaveNeo4jTest {
@@ -17,6 +19,17 @@ public class SaveNeo4jTest {
 
     @Test
     public void saveResource() {
+
+        for(Resource resource : resourceRepository.findAll()) {
+            ArrayList<String> oldFeature = resource.getFeature();
+            if(oldFeature.contains("At most 20 operations")) {
+                oldFeature.remove("At most 20 operations");
+            }else if(!oldFeature.contains("At most 20 operations")) {
+                oldFeature.add("At most 20 operations");
+            }
+            resource.setFeature(oldFeature);
+            resourceRepository.save(resource);
+        }
 
     }
 }
